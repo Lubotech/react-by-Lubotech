ed3e2db0e4b4b36d2bded0cf4330317743996ce9
Lubotech <luboyerajoel573@gmail.com>
2024-09-06 11:24:39 +0300

    Started and setup the react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+  <h1> Class React app </h1>
+  <Youtube />
+   </>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import Youtube from "./Youtube"; // Corrected file name casing
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h>Vite react app</h>
+    <Youtube />
+    </>
+  )
+}
+
+export default App
